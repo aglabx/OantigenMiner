878b3814ccd43c16bccfef92f75d6a224583507a
tniapp <tniapp@yandex.ru>
2023-08-12 14:45:55 +0000

    all till operon mapper ready

--- bakta_loaddb.sh
+++ bakta_loaddb.sh
@@ -7,7 +7,11 @@ else
     echo "Started loading bakta database"
     wget https://zenodo.org/record/7669534/files/db-light.tar.gz
     tar -xzf db-light.tar.gz
-    rm db-light.tar.gz
+    # rm db-light.tar.gz
 fi
 
-./amrfinder_update --force_update --database db-light/amrfinderplus-db/
+if [ -d "db-light/amrfinderplus-db" ]; then
+    echo amrfinderplus-db exists too
+else
+    amrfinder_update --force_update --database db-light/amrfinderplus-db/
+fi
